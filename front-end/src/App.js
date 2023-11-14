3a9c16df147e02c4c866d9ab9ad6d4846b6f7185
robert.downer <Robert.Downer@fdmgroup.com>
2023-11-14 14:56:02 +0000

    Added navbar and basic page setup

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from 'react-router-dom'
+import Homepage from './pages/HomePage';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+import Navbar from './components/Navbar';
+import LoadLanguages from './components/LoadLanguages';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element = {<Homepage/>}/>
+        <Route path="/languages" element = {<LoadLanguages/>}/>
+      </Routes>
+
+   
     </div>
   );
 }
